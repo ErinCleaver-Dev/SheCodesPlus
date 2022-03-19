ddfb970aaaa5e8accb34486d5aa6e8650bc3d1fd
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-03-19 06:53:49 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 let price = 4.5;
+let values = [4, 1, 3, 10]
+
+
+let rounded = Math.round(price);
+console.log(`Rounded: ${rounded}`);
+let ceil = Math.ceil(price);
+console.log(`Ceil: ${ceil}`)
+let floor = Math.floor(price);
+console.log(`Floor: ${floor}`)
+
+let min = Math.min(...values)
+console.log(`Min: ${min}`)
+let max = Math.max(...values)
+console.log(`Max: ${max}`)
